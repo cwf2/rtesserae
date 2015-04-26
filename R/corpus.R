c6344b654fa79f74a3e0bffc4883ee05c319c839
Chris Forstall <cforstall@gmail.com>
2015-04-26 15:15:52 +0200

    use environments as pseudo-objects

--- R/corpus.R
+++ R/corpus.R
@@ -7,7 +7,7 @@ tesre$word <- "\\w+"
 tesre$pbound <- "([:;\\.!\\?]+[”\"]?\\s*)"
 tesre$notseen <- "::::"
 
-parse.phrase <- function(text, phraseid, unitid) {
+parse.phrase <- function(text, tokenid, phraseid, unitid) {
   wtokens <- unlist(strsplit(x=text, split=tesre$nonword))
   ptokens <- unlist(strsplit(x=text, split=tesre$word))
   if (wtokens[1] == "") {
@@ -21,25 +21,39 @@ parse.phrase <- function(text, phraseid, unitid) {
   
   tokens <- c(rbind(ptokens, wtokens), finalp)
   types <- c(rep(c("P", "W"), length(wtokens)), "P")
-  not.empty <- tokens != ""
+  not.empty <- tokens != "" & ! is.na(tokens)
   
   data.table(
     display = tokens[not.empty],
     form = standardize(tokens[not.empty]),
     type = types[not.empty],
+    tokenid = seq(from=tokenid, length.out=sum(not.empty)),
     unitid = rep(unitid, sum(not.empty)),
     phraseid = rep(phraseid, sum(not.empty))
   )
 }
 
 ingest.text <- function(file) {
+  # create an environment (i.e. pseudo-object) 
+  # to hold this text and its features
+  text.object <- new.env()
+  
+  # store the original file name
+  assign("file", file, envir=text.object)
+  
+  # read the xml
+  cat("Reading", file, "\n")
   xml <- xmlParse(file=file)
   
-  nnodes <- length(xpathApply(xml, "//TextUnit", function(x){1}))
+  # get the loci
+  loc <- xpathSApply(xml, "//TextUnit", function(node) {xmlGetAttr(node, "loc")})
+  assign("loc", loc, envir=text.object)
+  nnodes <- length(loc)
   
-  cat("Reading", file, "\n")
+  # get the text
   pb <- txtProgressBar(min=1, max=nnodes, style=3)
   phraseid = 1
+  tokenid = 1
   
   token.table <- rbindlist(
     xpathApply(xml, "//TextUnit", function(node){
@@ -51,8 +65,9 @@ ingest.text <- function(file) {
       
       tokens.this.textunit <- rbindlist( 
         lapply(phrases, function(p) {
-          tokens.this.phrase <- parse.phrase(p, phraseid=phraseid, unitid=unitid)
+          tokens.this.phrase <- parse.phrase(p, tokenid=tokenid, phraseid=phraseid, unitid=unitid)
           phraseid <<- phraseid + 1
+          tokenid <<- tokenid + nrow(tokens.this.phrase)
           return(tokens.this.phrase)
         })
       )
@@ -69,46 +84,46 @@ ingest.text <- function(file) {
   token.table$type = as.factor(token.table$type)
   
   setkey(token.table, unitid, phraseid)
-  return(token.table)
+  
+  assign("tokens", token.table, envir=text.object)
+  return(text.object)
 }
 
-add.column <- function(index) {
-  wtok <- which(index$type=="W")
-  wtok <- data.frame(id=wtok, tok=index[wtok, form], stringsAsFactors=F)
-  wtok <- wtok[grepl("[a-z]", wtok$tok),]
-  utok <- unique(wtok$tok)
-  hash <- new.env(hash=T, size=length(utok))
-  for (tok in utok) { assign(tok, integer(0), envir=hash)}
+add.column <- function(text) {
+  forms <- unique(text$tokens[type=="W", form])
   
-  append.tok.id <- Vectorize(function(id, tok) {
-    ids <- get(tok, envir=hash)
-    ids <- c(ids, id)
-    assign(tok, ids, envir=hash)
-  })
+  assign("index.form", new.env(hash=T, size=length(forms)), envir=text)
   
-  do.call(append.tok.id, wtok)
-  return(hash)
+  f <- function(form, tokenid) {
+    assign(form, c(tokenid), envir=text$index.form)
+  }
+  
+  text$tokens[type=="W", f(form, tokenid), by=form, ]
+  
+  return(text)
 }
 
 feat <- function(form, feature) {
   return(unlist(mget(x=form, envir=feature, ifnotfound=form)))
 }
 
-add.col.feature <- function(index.token, dict.feature) {
-  forms <- ls(index.token)
-  index.feature <- new.env(hash=T, size=length(forms))
+add.col.feature <- function(text, feat.name, feat.dict) {
+  forms <- ls(text$index.form)
+  
+  index.feature <- new.env(hash=T)
+  assign(paste("index", feat.name, sep="."), index.feature, envir=text)
   
   cat("Stemming", length(forms), "forms\n")
   pb <- txtProgressBar(min=1, max=length(forms), style=3)
   
-  for (f in forms) {
-    indexable <- feat(f, feature=dict.feature)
-    tokids.form <- get(f, index.token)
+  for (form in forms) {
+    features <- feat(form, feature=feat.dict)
+    tokids.form <- get(form, text$index.form)
     
-    for (i in indexable) {
-      tokids.feature <- unlist(mget(i, envir=index.feature, mode="integer", ifnotfound=list(integer(0))))
+    for (feature in features) {
+      tokids.feature <- unlist(mget(feature, envir=index.feature, mode="integer", ifnotfound=list(integer(0))))
       
-      assign(i, value=c(tokids.feature, tokids.form), envir=index.feature)
+      assign(feature, value=c(tokids.feature, tokids.form), envir=index.feature)
     }
     setTxtProgressBar(pb, getTxtProgressBar(pb)+1)
   }
@@ -117,13 +132,13 @@ add.col.feature <- function(index.token, dict.feature) {
   cat("Removing duplicate entries\n")
   pb <- txtProgressBar(min=1, max=length(ls(index.feature)), style=3)
   
-  for (i in ls(index.feature)) {
-    assign(i, value=unique(get(i, index.feature)), envir=index.feature)
+  for (feature in ls(index.feature)) {
+    assign(feature, unique(get(feature, index.feature)), envir=index.feature)
     setTxtProgressBar(pb, getTxtProgressBar(pb)+1) 
   }
   close(pb)
   
-  return(index.feature)
+  return(text)
 }
 
 links <- function(s, t, stoplist=character(0)) {
@@ -133,7 +148,7 @@ links <- function(s, t, stoplist=character(0)) {
   links <- rbindlist(
     lapply(features, function(f) {
       setTxtProgressBar(pb, getTxtProgressBar(pb)+1)
-      expand.grid(s=get(f, envir=s), t=get(f, envir=t), f=f)
+      expand.grid(s.tokenid=get(f, envir=s), t.tokenid=get(f, envir=t), feature=f)
     })
   )
   
@@ -195,5 +210,58 @@ feature.stoplist <- function(freq.list, n=10) {
   } 
   
   return(stoplist)
+}
+
+tess.search <- function(s.text, t.text, feat.name="form") {
+  cat("Calculating feature frequencies\n")
+  
+  s.feat <- get(paste("index", feat.name, sep="."), envir=s.text)
+  t.feat <- get(paste("index", feat.name, sep="."), envir=t.text)
+  
+  s.freq <- feature.frequencies(s.feat)
+  t.freq <- feature.frequencies(t.feat)
+  
+  stoplist <- feature.stoplist(list(s.freq, t.freq))
+  cat("Stoplist:", paste(stoplist, collapse=" "), "\n")
+  
+  cat("Generating links\n")
+  result <- links(s.feat, t.feat, stoplist)
+  result <- cbind(result,
+    s.unitid=s.text$tokens[result$s.tokenid, unitid], 
+    t.unitid=t.text$tokens[result$t.tokenid, unitid]
+  )
+  
+  check.n.forms <- function(s.tokenid, t.tokenid) {
+    forms <- unique(unlist(c(s.text$tokens[s.tokenid, form], t.text$tokens[t.tokenid, form])))
+    length(forms)
+  }
+  
+  cat ("Checking minimal match criteria:\n")
+  # for each pair of phrases, make sure at least two source tokens
+  cat (" [1/4] at least 2 source tokens\n")
+  setkey(result, s.unitid, t.unitid, s.tokenid)
+  m <- result[! duplicated(result), .N, by=.(s.unitid, t.unitid)]
+  m <- m[N>1, .(s.unitid, t.unitid)]
+  result <- result[m]
+  
+  # make sure at least two target tokens
+  cat (" [2/4] at least 2 target tokens\n")
+  setkey(result, s.unitid, t.unitid, t.tokenid)
+  m <- result[! duplicated(result), .N, by=.(s.unitid, t.unitid)]
+  m <- m[N>1, .(s.unitid, t.unitid)]
+  result <- result[m]
+  
+  # make sure at least 2 features
+  cat (" [3/4] at least 2 features\n")
+  setkey(result, s.unitid, t.unitid, feature)
+  m <- result[! duplicated(result), .N, by=.(s.unitid, t.unitid)]
+  m <- m[N>1, .(s.unitid, t.unitid)]
+  result <- result[m]
+  
+  # make sure at least two differently inflected forms
+  #  cat (" [4/4] at least 2 forms\n")
+  #  result <- result[result[,check.n.forms(s,t), by=.(unit_s, unit_t)][V1>1], .(s, t, f, unit_s, unit_t)]
+  
+  return(result)
 }
 
--- R/lucan-vergil.R
+++ R/lucan-vergil.R
@@ -1,62 +1,20 @@
 source(file.path("R", "corpus.R"))
 source(file.path("R", "stem.R"))
 
-s.text <- ingest.text("tesscorpus/vergil.aeneid.xml")
-t.text <- ingest.text("tesscorpus/lucan.bellum_civile.xml")
+s <- ingest.text("tesscorpus/vergil.aeneid.xml")
+t <- ingest.text("tesscorpus/lucan.bellum_civile.xml")
 
-s.word <- add.column(s.text)
-t.word <- add.column(t.text)
+add.column(s)
+add.column(t)
 
-s.stem <- add.col.feature(s.word, stems)
-t.stem <- add.col.feature(t.word, stems)
+add.col.feature(s, "stem", stems)
+add.col.feature(t, "stem", stems)
 
-tess.search <- function(s.feat, t.feat, s.word, t.word) {
-  cat("Calculating feature frequencies\n")
-  s.freq <- feature.frequencies(s.feat)
-  t.freq <- feature.frequencies(t.feat)
+result <- tess.search(s, t)
 
-  stoplist <- feature.stoplist(list(s.freq, t.freq))
-  cat("Stoplist:", paste(stoplist, collapse=" "), "\n")
-  
-  cat("Generating links\n")
-  result <- links(s.feat, t.feat, stoplist)
-  result <- cbind(result,
-    unit_s=s.text[result$s, unitid], 
-    unit_t=t.text[result$t, unitid]
-  )
-  
-  check.n.forms <- function(s, t) {
-    forms <- unique(unlist(c(s.text[s, form], t.text[t, form])))
-    length(forms)
-  }
-  
-  cat ("Checking minimal match criteria:\n")
-  # for each pair of phrases, make sure at least two source tokens
-  cat (" [1/4] at least 2 source tokens\n")
-  setkey(result, unit_s, unit_t, s)
-  m <- result[! duplicated(result), .N, by=.(unit_s, unit_t)]
-  m <- m[N>1, .(unit_s, unit_t)]
-  result <- result[m]
-  
-  # make sure at least two target tokens
-  cat (" [2/4] at least 2 target tokens\n")
-  setkey(result, unit_s, unit_t, t)
-  m <- result[! duplicated(result), .N, by=.(unit_s, unit_t)]
-  m <- m[N>1, .(unit_s, unit_t)]
-  result <- result[m]
-  
-  # make sure at least 2 features
-  cat (" [3/4] at least 2 features\n")
-  setkey(result, unit_s, unit_t, f)
-  m <- result[! duplicated(result), .N, by=.(unit_s, unit_t)]
-  m <- m[N>1, .(unit_s, unit_t)]
-  result <- result[m]
-  
-  # make sure at least two differently inflected forms
-#  cat (" [4/4] at least 2 forms\n")
-#  result <- result[result[,check.n.forms(s,t), by=.(unit_s, unit_t)][V1>1], .(s, t, f, unit_s, unit_t)]
-  
-  return(result)
-}
+# check against benchmark
+setkey(result, t.unitid, s.unitid)
+result.loc <- result[,.(t.loc=t$loc[t.unitid], s.loc=s$loc[s.unitid]), by=.(t.unitid, s.unitid)][,.(t.loc, s.loc),]
 
-
+bench <- fread("data/lucan-vergil.word.txt")
+bench.loc <- bench[,.(t.loc=sub(".+\\s", "", TARGET_LOC), s.loc=sub(".+\\s", "", SOURCE_LOC), feat=SHARED),][,.(t.loc, s.loc),]
